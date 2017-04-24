3a8bb27452124ff71b8de0d5328f20e0bb1f22a0
LyneLiu <595596226@qq.com>
2017-04-24 13:41:16 +0800

    add spring context part

--- src/main/java/com/lyne/SpringBootTomcatDemoApplication.java
+++ src/main/java/com/lyne/SpringBootTomcatDemoApplication.java
@@ -1,17 +1,31 @@
 package com.lyne;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class SpringBootTomcatDemoApplication extends SpringBootServletInitializer {
+public class SpringBootTomcatDemoApplication extends SpringBootServletInitializer implements ApplicationContextAware {
 
+
+    private static ApplicationContext applicationContext; // Spring应用上下文环境
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
 
     @Override
     protected SpringApplicationBuilder configure(
@@ -27,4 +41,6 @@ public class SpringBootTomcatDemoApplication extends SpringBootServletInitialize
     public String index() {
         return "spring boot demo with tomcat!";
     }
+
+
 }
